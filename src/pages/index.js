22b3b17f7ab7419adbb6b81ff3e5da508dcb40a7
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2019-02-10 13:42:36 +0800

    Updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,17 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
+  <div>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>
+      <div style={{ marginBottom: '5rem' }}>
+        <h1>Noel Earvin Piamonte</h1>
+        <p>
+          I build websites for <a href="http://bentacos.com" target="_blank">Bentacos</a> and recently into mobile app development.
+          </p>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  </div>
 )
 
 export default IndexPage
