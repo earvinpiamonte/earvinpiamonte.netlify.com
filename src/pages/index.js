d9fd502fa012c4b020eb462cb7048be5dece1a45
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2019-03-03 15:09:31 +0800

    Updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,52 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <div>
-    <SEO title="Home" keywords={[`website developer`, `baguio`, `philippines`]} />
-    <div>
-      <div style={{ marginBottom: '5rem' }}>
-        <h1>Noel Earvin Piamonte</h1>
-        <p className="p-container">
-          I&apos;m currently living in Mandaluyong City and working as a Web Developer for <a href="http://bentacos.com" target="_blank">Bentacos</a>.
+class IndexPage extends React.Component{
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      easterEggKeyTriggerCount: 0
+    };
+
+    document.addEventListener('keyup', (event) => {
+      if (event.which === 82) {
+
+        this.setState((prevState) => (
+          {
+            easterEggKeyTriggerCount: prevState.easterEggKeyTriggerCount + 1
+          }
+        ));
+
+        if (this.state.easterEggKeyTriggerCount === 4) {
+          this.setState({
+            easterEggKeyTriggerCount: 0
+          });
+          alert('You found an easter egg!\nThe letter is R.');
+        }
+
+        console.log(this.state.easterEggKeyTriggerCount);
+      }
+    });
+
+  }
+
+  render(){
+    return(
+      <div>
+        <SEO title="Home" keywords={[`website developer`, `baguio`, `philippines`]} />
+        <div>
+          <div style={{ marginBottom: '5rem' }}>
+            <h1>Noel Earvin Piamonte</h1>
+            <p className="p-container">
+              I&apos;m currently living in Mandaluyong City and working as a Web Developer for <a href="http://bentacos.com" target="_blank">Bentacos</a>.
           </p>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-)
+    );
+  }
+}
 
 export default IndexPage
