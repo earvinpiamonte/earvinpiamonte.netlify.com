a9727bcc85889dda7f214d39333674cbe9c6ebd6
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2019-05-13 12:12:38 +0800

    Updated text and Resume URL

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-class IndexPage extends React.Component{
+class IndexPage extends React.Component {
 
   constructor(props) {
     super(props);
@@ -26,14 +26,15 @@ class IndexPage extends React.Component{
         }
       ));
 
-      if (this.state.easterEggKeyTriggerCount === 4) {document.addEventListener('keyup', this.easterEggKeyTrigger);
+      if (this.state.easterEggKeyTriggerCount === 4) {
+        document.addEventListener('keyup', this.easterEggKeyTrigger);
         this.setState({
           easterEggKeyTriggerCount: 0
         });
-        alert('You found an easter egg!\nR.V.C.');
+        // alert('You found an easter egg!\nR.V.C.');
       }
 
-      console.log(this.state.easterEggKeyTriggerCount);
+      // console.log(this.state.easterEggKeyTriggerCount);
     }
   }
 
@@ -46,23 +47,20 @@ class IndexPage extends React.Component{
     document.removeEventListener('keyup', this.easterEggKeyTrigger);
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
-        <SEO title="Home" keywords={[`website developer`, `baguio`, `philippines`]} />
+        <SEO title="Website Developer" keywords={[`website developer`, `baguio`, `philippines`]} />
         <div>
           <div style={{ marginBottom: '5rem' }}>
             <h1>Noel Earvin Piamonte</h1>
             <p className="p-container">
-              I develop websites for <a href="https://bentacos.com" target="_blank">Bentacos</a>, previously an independent website developer at <a href="https://www.freelancer.com/u/earvinpiamonte" target="_blank">Freelancer.com</a>.
+              I develop websites for <a href="https://bentacos.com/" target="_blank">Bentacos</a>. Previously, I&apos;m an independent website developer at <a href="https://www.freelancer.com/u/earvinpiamonte/" target="_blank">Freelancer.com</a>.
             </p>
-            <p style={{marginTop: '2rem'}}>
-              <a href="https://drive.google.com/file/d/1s2CxgkXgjZiePAcaP0JX98huAM6wJomN/view" target="_blank" class="site-btn" title="View Résumé">
-                  <small>View Résumé</small>
-                </a>
-            </p>
-            <p style={{ color: '#000' }}>
-              R.V.C.
+            <p style={{ marginTop: '2rem' }}>
+              <a href="https://drive.google.com/open?id=1fyKTOnutzM3Ke5pFpzto0L1FQYeXeVBp" target="_blank" class="site-btn">
+                <small>View Résumé</small>
+              </a>
             </p>
           </div>
         </div>
