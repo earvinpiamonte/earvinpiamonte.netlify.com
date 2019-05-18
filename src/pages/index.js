dfa123f2f768a016ab73a8f044cc06befa1ccdaf
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2019-05-18 12:50:03 +0800

    Updated home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,50 @@
 import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
 import SEO from "../components/seo"
 
 class IndexPage extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      easterEggKeyTriggerCount: 0
+    };
+
+    this.easterEggKeyTrigger = this.easterEggKeyTrigger.bind(this);
+  }
+
+  easterEggKeyTrigger(event) {
+    if (event.which === 82) {
+
+      this.setState((prevState) => (
+        {
+          easterEggKeyTriggerCount: prevState.easterEggKeyTriggerCount + 1
+        }
+      ));
+
+      if (this.state.easterEggKeyTriggerCount === 4) {
+        document.addEventListener('keyup', this.easterEggKeyTrigger);
+        this.setState({
+          easterEggKeyTriggerCount: 0
+        });
+        // alert('You found an easter egg!\nR.V.C.');
+      }
+
+      // console.log(this.state.easterEggKeyTriggerCount);
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.easterEggKeyTrigger);
+
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.easterEggKeyTrigger);
   }
 
   render() {
